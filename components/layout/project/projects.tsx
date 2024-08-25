e9691f65188e26911214c2bd2e65da9a9ff4d3d3
forenoo <forenofaisal21@gmail.com>
2024-08-25 10:37:02 +0700

    refactor: Remove unused `formatDateToYear` function from carousel.tsx

--- components/layout/project/projects.tsx
+++ components/layout/project/projects.tsx
@@ -1,16 +1,10 @@
 import ProjectCard from "@/components/ui/project-card";
-import { prisma } from "@/lib/prisma";
 
-export default async function Projects() {
-  const projectData = await prisma.project.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+export default function Projects() {
 
   return (
     <section className="mx-auto max-w-full columns-1 space-y-6 sm:block sm:columns-2 sm:gap-6 lg:columns-3 xl:columns-4">
-      {projectData.map((project, index) => (
+      {.map((project, index) => (
         <ProjectCard
           key={index}
           image={project.image}
